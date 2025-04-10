d51574acc280cc94d24f74d79d94664acd71049b
vikramkonidhala <vikramkonidhala@gmail.com>
2025-04-10 08:47:12 +0530

    Changes in Firm and Product Controller

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads/");
+    cb(null, "../uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
