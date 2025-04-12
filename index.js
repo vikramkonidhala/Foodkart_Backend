1d8dd4e4693225e47e1da2866a089b6e038e767d
vikramkonidhala <vikramkonidhala@gmail.com>
2025-04-12 11:03:04 +0530

    Added Cloudionary for Images

--- index.js
+++ index.js
@@ -18,7 +18,21 @@ if (!fs.existsSync(uploadsDir)) {
 const app = express();
 
 dotEnv.config();
-app.use(cors());
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");;
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
